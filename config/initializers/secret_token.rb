db04092cb5aeed37d20b600d69a17788204269cf
Travis Whittaker <travis.whittaker115@gmail.com>
2014-04-10 15:56:25 -0400

    Something something heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AutoplaylistServer::Application.config.secret_key_base = '2009a99454ed454c017f08487c34a9b8db82435ca904e395f20ce7f3b81be9a0a661b64873697c18e8779f92d1caeebbcfb391561cd5ab8b17ca9dd62f848518'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AutoplaylistServer::Application.config.session_store :cookie_store, key: '_autoplaylist-server_session'
